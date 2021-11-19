068e2ccc29fda688129b56f1ddafd9b4ca6efe77
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-19 20:06:25 -0300

    stacks e elementos básicos

--- IntroSwiftUI/ContentView.swift
+++ IntroSwiftUI/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        // vertical
+        VStack() {
+            Text("Hello!")
+                .padding(10)
+                .font(.title)
+                .background(Color(red: 157/255, green: 120/255, blue: 234/255))
+                .cornerRadius(8.0)
+            
+            Button(action: {}) {
+                Text("Clique aqui")
+                    .foregroundColor(.red)
+                    .bold()
+            }
+        }
+        
+        // horizontal
+        HStack(alignment: .top, spacing: 32) {
+            Image("ray")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        
+        // um em cima do outro -> utilizar quando quer colocar uma cor de fundo por exemplo
+        ZStack() {
+            
+            Color.blue.ignoresSafeArea()
+            
+            Text("Hello World!")
+                .foregroundColor(.white)
+                .font(.title)
+        }
+        
     }
 }
 
