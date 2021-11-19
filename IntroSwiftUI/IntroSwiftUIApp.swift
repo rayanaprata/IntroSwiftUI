3e3a9605e9486a8dc0fc31d9438f611ba51232d6
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-19 19:47:50 -0300

    Initial Commit

---
+++ IntroSwiftUI/IntroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSwiftUIApp.swift
+//  IntroSwiftUI
+//
+//  Created by Rayana Prata Neves on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
